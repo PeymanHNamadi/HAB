558be83c265001850159a849ace672ece821249f
PeymanHNamadi <99760532+PeymanHNamadi@users.noreply.github.com>
2026-05-27 12:56:09 -0700

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 # assume that the container already has activated the right environment
 # assume current working directory is at the top level of this repo
-panel serve habdashboardv2.ipynb --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
+panel serve habdashboard.ipynb --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
